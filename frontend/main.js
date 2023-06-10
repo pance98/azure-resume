cedd04bb6193465119bd2d294e439261176280b5
panche <cvetkovski_pance@yahoo.com>
2023-06-10 06:10:39 +0200

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
-        return response.json
+        return response.json()
     }).then(response => {
         console.log("Website called function API");
         count = response.count;
