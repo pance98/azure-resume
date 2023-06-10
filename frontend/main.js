bb66f0efc9d9d637969f4cc1ad5d3bde1d6307a9
panche <cvetkovski_pance@yahoo.com>
2023-06-10 10:25:01 +0200

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://resume-counter-fun.azurewebsites.net/api/GetResumeCounter?code=RqcDD_BoXMI3_vF_jsiyu2G-7GOZ4PTbb8RgxL1GXB4SAzFuMCY59A==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API");
